de2c75f79ea090febd15479118fa7fdf21ce6d08
Felix Rutsch <rutsch@cps.tu-darmstadt.de>
2025-05-09 15:37:21 +0200

    add linear version euler implicit

--- 01_time_integration/planar_pendulum.m
+++ 01_time_integration/planar_pendulum.m
@@ -3,10 +3,10 @@ clearvars
 close all
 
 % Parameters
-g = 9.81;               % gravity (m/s^2)
-L = 1.0;                % length of pendulum (m)
-delta_t = 0.05;         % time step
-T = 10;                 % total time (s)
+g = 9.81;               % gravity [m/s^2]
+L = 1.0;                % length of pendulum [m]
+delta_t = 0.05;         % time step [s]
+T = 10;                 % total time [s]
 N = round(T/delta_t);   % number of time steps
 
 % Preallocate
@@ -14,72 +14,83 @@ phi = zeros(1, N+1);
 v = zeros(1, N+1);
 t = 0:delta_t:T;
 
-% Initial conditions
-phi(1) = pi/4;  % 45 degrees
-v(1) = 0;
+% Initial conditions 
+phi(1) = pi/4;          % [rad] e.g. pi/4 for 45 degrees
+v(1) = 0;               % [rad/s]
 
-method = 'explicit Euler';
-linearized = false;
+method = 'implicit Euler';
+linearized = true;
 
 switch method
-    case 'explicit Euler'
-        for n=1:N
-            phi(n+1) = phi(n) + v(n)*delta_t;
-
-            if linearized
-            v(n+1) = v(n) - g/L*phi(n)*delta_t;
-            else
-            v(n+1) = v(n) - g/L*sin(phi(n))*delta_t; 
-            end
-        end
-
-    case 'implicit Euler'
-    for n = 1:N
-        % Initial guess for Newton iteration
-        phi_guess = phi(n);
-        v_guess = v(n);
-
-        % TODO linearisierte variante 
-
-        % Newton iteration
-        for iter = 1:10
-            % Residuals
-            F1 = phi_guess - phi(n) - delta_t * v_guess;
-            F2 = v_guess - v(n) + delta_t * (g/L) * sin(phi_guess);
-
-            % Jacobian
-            J = [1,        -delta_t;
-                 delta_t*(g/L)*cos(phi_guess), 1];
-
-            % Update
-            delta = -J \ [F1; F2];
-            phi_guess = phi_guess + delta(1);
-            v_guess = v_guess + delta(2);
-
-            if norm(delta) < 1e-10
-                break;
-            end
-        end
-
-        % Store updated values
-        phi(n+1) = phi_guess;
-        v(n+1) = v_guess;
-    end 
-
-    case 'symplectic Euler'
+case 'explicit Euler' %----------------------------------------------------
     for n=1:N
-
         phi(n+1) = phi(n) + v(n)*delta_t;
 
         if linearized
-            v(n+1) = v(n) - g/L*phi(n+1)*delta_t;
+            v(n+1) = v(n) - g/L*phi(n)*delta_t;
         else
-            v(n+1) = v(n) - g/L*sin(phi(n+1))*delta_t; 
+            v(n+1) = v(n) - g/L*sin(phi(n))*delta_t; 
         end
     end
 
-    otherwise
-        error('unknown method name')
+case 'implicit Euler' %----------------------------------------------------
+    for n = 1:N
+        
+        if linearized % linear system of equations        
+            A = [1,-delta_t;
+                 g/L*delta_t, 1];
+            p = A \ [phi(n); v(n)];
+            phi(n+1) = p(1);
+            v(n+1) = p(2);
+    
+        else % non-linear system of equations 
+
+            % Initial guess for Newton iteration
+            phi_guess = phi(n);
+            v_guess = v(n);
+    
+            % Newton iteration
+            for iter = 1:10
+                % Residuals
+                F1 = phi_guess - phi(n) - delta_t * v_guess;
+                F2 = v_guess - v(n) + delta_t * (g/L) * sin(phi_guess);
+    
+                % Jacobian
+                J = [1,        -delta_t;
+                     delta_t*(g/L)*cos(phi_guess), 1];
+    
+                % Update
+                delta = -J \ [F1; F2];
+                phi_guess = phi_guess + delta(1);
+                v_guess = v_guess + delta(2);
+    
+                if norm(delta) < 1e-10
+                    break;
+                end
+            end
+    
+            % Store updated values
+            phi(n+1) = phi_guess;
+            v(n+1) = v_guess;
+
+        end
+    end 
+
+case 'symplectic Euler' %--------------------------------------------------
+for n=1:N
+
+    phi(n+1) = phi(n) + v(n)*delta_t;
+
+    if linearized
+        v(n+1) = v(n) - g/L*phi(n+1)*delta_t;
+    else
+        v(n+1) = v(n) - g/L*sin(phi(n+1))*delta_t; 
+    end
+end
+
+% -------------------------------------------------------------------------
+otherwise
+    error('unknown method name')
 
 end
 
